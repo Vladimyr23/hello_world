c4673bbd4114aa633ca4713ead905f19e47a4a20
Vladimyr23 <33516849+Vladimyr23@users.noreply.github.com>
2019-09-28 14:11:11 +0100

    Add files via upload

---
+++ Assesm_PasswordGenerator/Form1.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBoxNote = New System.Windows.Forms.TextBox()
+        Me.lblTime_Date = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnHelp = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtBoxNote
+        '
+        Me.txtBoxNote.Location = New System.Drawing.Point(6, 19)
+        Me.txtBoxNote.MaxLength = 22
+        Me.txtBoxNote.Name = "txtBoxNote"
+        Me.txtBoxNote.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtBoxNote.Size = New System.Drawing.Size(268, 22)
+        Me.txtBoxNote.TabIndex = 1
+        '
+        'lblTime_Date
+        '
+        Me.lblTime_Date.AutoSize = True
+        Me.lblTime_Date.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTime_Date.Location = New System.Drawing.Point(14, 71)
+        Me.lblTime_Date.Name = "lblTime_Date"
+        Me.lblTime_Date.Size = New System.Drawing.Size(142, 16)
+        Me.lblTime_Date.TabIndex = 2
+        Me.lblTime_Date.Text = "Current Time and Date"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(17, 26)
+        Me.TextBox1.MaxLength = 2
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(41, 31)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(64, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(19, 25)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "-"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(136, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 25)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "-"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(89, 26)
+        Me.TextBox2.MaxLength = 2
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(41, 31)
+        Me.TextBox2.TabIndex = 6
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(208, 29)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(19, 25)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "-"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(161, 26)
+        Me.TextBox3.MaxLength = 2
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(41, 31)
+        Me.TextBox3.TabIndex = 8
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(233, 26)
+        Me.TextBox4.MaxLength = 2
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(41, 31)
+        Me.TextBox4.TabIndex = 10
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGenerate.Location = New System.Drawing.Point(183, 71)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(91, 23)
+        Me.btnGenerate.TabIndex = 11
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(137, 184)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 12
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(218, 184)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 13
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnHelp
+        '
+        Me.btnHelp.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHelp.Location = New System.Drawing.Point(9, 184)
+        Me.btnHelp.Name = "btnHelp"
+        Me.btnHelp.Size = New System.Drawing.Size(75, 23)
+        Me.btnHelp.TabIndex = 14
+        Me.btnHelp.Text = "Help"
+        Me.btnHelp.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtBoxNote)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 118)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(284, 49)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Generated password note:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnGenerate)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.lblTime_Date)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(9, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(284, 100)
+        Me.GroupBox2.TabIndex = 16
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Passcode:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(298, 216)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnHelp)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSave)
+        Me.HelpButton = True
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Password Generator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents txtBoxNote As TextBox
+    Friend WithEvents lblTime_Date As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnHelp As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+End Class
---
+++ Assesm_PasswordGenerator/Form1.vb
@@ -0,0 +1,145 @@
+﻿Public Class Form1
+    'App path that used to make the saved text file to be placed at the same place with app executable file
+    Dim Application_Path As String = Application.ExecutablePath()
+    'String Value that contant all the information to be saved 
+    Dim save_string As String
+    'values to check validation of the data entered into the password fields
+    Dim ch(99) As Char
+    Dim len As Integer
+
+    'Current time display arrangement
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim TimerTest As Timer = New Timer
+        TimerTest.Interval = 1000
+        AddHandler TimerTest.Tick, AddressOf Time_Tick
+        TimerTest.Start()
+        TextBox1.Text = Format("00", "00")
+        TextBox2.Text = Format("00", "00")
+        TextBox3.Text = Format("00", "00")
+        TextBox4.Text = Format("00", "00")
+
+    End Sub
+
+    'Label to display the current date/time
+    Private Sub Time_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        lblTime_Date.Text = DateTime.Now.ToString()
+    End Sub
+
+    'Random number generator for each textBox
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+
+        Randomize()
+
+        TextBox1.Text = Format(CInt(Int(100 * Rnd())), "00")
+        TextBox2.Text = Format(CInt(Int(100 * Rnd())), "00")
+        TextBox3.Text = Format(CInt(Int(100 * Rnd())), "00")
+        TextBox4.Text = Format(CInt(Int(100 * Rnd())), "00")
+    End Sub
+
+    'Button to exit the app
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+        End
+    End Sub
+
+    'Next part saves the Time when the button 'Save' pressed, Note to the password and the generated code also it replaces the empty characters with 0 (where 5 replaced with 05)
+    'with the EOL at the end to make next password saved from the new line
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        If TextBox1.Text.Length = 1 Then
+            TextBox1.Text = "0" + TextBox1.Text
+        ElseIf TextBox1.Text.Length = 0 Then
+            TextBox1.Text = "00"
+        End If
+        Format(TextBox1.Text, "00")
+
+        If TextBox2.Text.Length = 1 Then
+            TextBox2.Text = "0" + TextBox2.Text
+        ElseIf TextBox2.Text.Length = 0 Then
+            TextBox2.Text = "00"
+        End If
+        Format(TextBox2.Text, "00")
+
+        If TextBox3.Text.Length = 1 Then
+            TextBox3.Text = "0" + TextBox3.Text
+        ElseIf TextBox3.Text.Length = 0 Then
+            TextBox3.Text = "00"
+        End If
+        Format(TextBox3.Text, "00")
+
+        If TextBox4.Text.Length = 1 Then
+            TextBox4.Text = "0" + TextBox4.Text
+        ElseIf TextBox4.Text.Length = 0 Then
+            TextBox4.Text = "00"
+        End If
+        Format(TextBox4.Text, "00")
+
+        save_string = lblTime_Date.Text + "::" + txtBoxNote.Text + "::" + TextBox1.Text + TextBox2.Text + TextBox3.Text + TextBox4.Text
+        My.Computer.FileSystem.WriteAllText(Application_Path + "saved_values.txt", save_string + "
+", True)
+
+        MsgBox("Generated code was added into the file: " + Application_Path + "Saved_values.txt" + " at " + lblTime_Date.Text, MessageBoxButtons.OK, "Save message")
+    End Sub
+
+    'This part of the program cheks if the value entred by user in a one of 4 passcode fields is NUMERIC value
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        len = TextBox1.Text.Length
+        ch = TextBox1.Text.ToCharArray()
+        For i = 0 To len - 1
+            If Not IsNumeric(ch(i)) Then
+                MsgBox("Value you are trying to insert is not numeric.", MsgBoxStyle.Exclamation, "Warning!")
+                TextBox1.Clear()
+            End If
+        Next
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        len = TextBox2.Text.Length
+        ch = TextBox2.Text.ToCharArray()
+        For i = 0 To len - 1
+            If Not IsNumeric(ch(i)) Then
+                MsgBox("Value you are trying to insert is not numeric.", MsgBoxStyle.Exclamation, "Warning!")
+                TextBox2.Clear()
+            End If
+        Next
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        len = TextBox3.Text.Length
+        ch = TextBox3.Text.ToCharArray()
+        For i = 0 To len - 1
+            If Not IsNumeric(ch(i)) Then
+                MsgBox("Value you are trying to insert is not numeric.", MsgBoxStyle.Exclamation, "Warning!")
+                TextBox3.Clear()
+            End If
+        Next
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        len = TextBox4.Text.Length
+        ch = TextBox4.Text.ToCharArray()
+        For i = 0 To len - 1
+            If Not IsNumeric(ch(i)) Then
+                MsgBox("Value you are trying to insert is not numeric.", MsgBoxStyle.Exclamation, "Warning!")
+                TextBox4.Clear()
+            End If
+        Next
+    End Sub
+
+    'Help button
+    Private Sub btnHelp_Click(sender As Object, e As EventArgs) Handles btnHelp.Click
+        MsgBox("Press 'Generate' button to generate a numeric 8 digit code in a format ##-##-##-## above the 'Generate' button.
+In case you would like to type your own numeric code, key it into the boxes or change digits in generated code.
+Empty fields will be replaced with zeros.
+To leave a note to the passcode type it in the note field.
+The note can be up to 22 symbols long.
+Finally, to save the passcode with current time/date and the 'Note', press 'Save' button at the bottom of app window.
+After 'Save' button pressed the message, pointing the text file name which is located at the root of main application file  will appear.",
+                MessageBoxButtons.OK, "Password generator Help")
+    End Sub
+End Class
